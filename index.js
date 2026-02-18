4cb6762173fabfd7d944cbdd5d6e87a16dbcefa7
eduardo <angeloeduardo_184@yahoo.com.br>
2026-02-18 18:17:52 -0300

    foi instalado e configurado o express

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('veio');
+import express from 'express';
+
+const app = express();
+
+app.get('/',(req,res) => {
+    res.send('<h1>Hello World</h1>');
+});
+
+app.listen(3000,()=>{
+    console.log('conectouuuu');
+});
