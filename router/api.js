f23edb57a8f8530c096e2cac418e2f39336f3a56
eduardo <angeloeduardo_184@yahoo.com.br>
2026-02-22 17:52:31 -0300

    ajuste já com swagger

--- router/api.js
+++ router/api.js
@@ -1,15 +1,19 @@
 import express from 'express';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from '../swagger.json' with { type: 'json' };
 import shortlink from '../src/controller/shortLink.js';
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/shorten/create',shortlink.create);
+// Swagger
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.post('/shorten',shortlink.create);
 app.get('/shorten/id/:id',shortlink.getFind);
 app.put('/shorten/id/:id',shortlink.update);
-app.get('/shorten/:url',shortlink.link);
 app.get('/shorten/date/:date_create',shortlink.find);
+app.get('/shorten/:url',shortlink.link);
 
 export default app;
--- src/controller/shortLink.js
+++ src/controller/shortLink.js
@@ -7,8 +7,9 @@ const shortlink = {
         try {
             
             if (!req.body?.url) {
-                return res.status(400).json({
-                    error: "O campo 'url' é obrigatório"
+                return res.status(400).json({error: "O campo obrigatório no corpo da requisição.",
+                    message: "O campo 'url' é obrigatório no corpo da requisição.",
+                    statusCode: 400
                 });
             }
 
@@ -25,13 +26,18 @@ const shortlink = {
                     {
                         url:shortUrl,
                     }    
-                ]
+                ],
+                statusCode:201
             });
 
         } catch (error) {
 
             return res.status(500).json({
-                error: "Erro ao criar link"
+                success: false,
+                error: {
+                    type: "INTERNAL_SERVER_ERROR",
+                    message: "Ocorreu um erro interno ao criar o link."
+                }
             });
         }
     },
@@ -39,14 +45,18 @@ const shortlink = {
     async getFind(req, res) {
 
         if (!req.params?.id) {
-            return res.status(404).json({
-                error: "nada foi encontrado"
+            return res.status(400).json({
+                error: "Parâmetro obrigatório não informado.",
+                message: "O parâmetro 'id' deve ser informado na URL.",
+                path: "/shorten/id/:id",
+                statusCode: 400
             });
         }
 
         const id = req.params?.id;
         res.status(200).json({
-            message:await shortLinkEntity.find('id',id)
+            message:await shortLinkEntity.find('id',id),
+            statusCode:200
         });
 
     },
@@ -54,14 +64,17 @@ const shortlink = {
     async find(req, res) {
 
         if (!req.params?.date_create) {
-            return res.status(404).json({
-                error: "nada foi encontrado"
+            return res.status(400).json({error: "Parâmetro obrigatório não informado.",
+                message: "O parâmetro 'date' deve ser informado na URL.",
+                path: "/shorten/date/:date",
+                statusCode: 400
             });
         }
 
         const dateCreate = req.params?.date_create;
         res.status(200).json({
-            message:await shortLinkEntity.findDate(dateCreate)
+            message:await shortLinkEntity.findDate(dateCreate),
+            statusCode:200
         });
 
     },
@@ -70,14 +83,17 @@ const shortlink = {
         try {
 
             if (!req.params?.id) {
-                return res.status(404).json({
-                    error: "nada foi encontrado"
+                return res.status(400).json({error: "Parâmetro obrigatório não informado.",
+                    message: "O parâmetro 'id' deve ser informado na URL.",
+                    path: "/shorten/id/:id",
+                    statusCode: 400
                 });
             }
 
             if (!req.body?.url) {
-                return res.status(404).json({
-                    error: "nada foi encontrado"
+                return res.status(400).json({error: "O campo obrigatório no corpo da requisição.",
+                    message: "O campo 'url' é obrigatório no corpo da requisição.",
+                    statusCode: 400
                 });
             }
 
@@ -97,7 +113,12 @@ const shortlink = {
             
         } catch (error) {
             return res.status(500).json({
-                error: "Erro ao atualizar o link"
+                success: false,
+                error: {
+                    type: "INTERNAL_SERVER_ERROR",
+                    message: "Ocorreu um erro interno ao atualizar o link."
+                },
+                statusCode:500
             });
         }
 
@@ -106,20 +127,27 @@ const shortlink = {
     async link(req, res) {
         try {
             if (!req.params?.url) {
-                return res.status(404).json({
-                    error: "nada foi encontrado"
+                return res.status(400).json({error: "Parâmetro obrigatório não informado.",
+                    error: "O parâmetro 'url' é obrigatório na rota.",
+                    statusCode:400
                 });
             }
     
             const url = req.params?.url;
             const baseUrl = process.env.BASE_URL || 'http://localhost:3000/';
             const link = await shortLinkEntity.find('url_short',baseUrl+'shorten/'+url);
+
             return res.redirect(302, link[0].url_original);
 
         } catch (error) {
 
             return res.status(500).json({
-                error: "Erro ao processar redirecionamento"
+                success: false,
+                error: {
+                    type: "INTERNAL_SERVER_ERROR",
+                    message: "Ocorreu um erro interno ao ao redirecionar."
+                },
+                statusCode:500
             });
         }
         
